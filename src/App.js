e84a74be39431fe97d7e0cbc7366cc6618e7f09b
Jan Maaß <git@antipost.de>
2023-11-13 11:18:27 +0100

    Create HelloWorldArticle function

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+export function HelloWorldArticle() {
+  return <article>
+    <h1>Hello Article</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium in quam. Quod incidunt temporibus sint, minus, dicta maiores optio eligendi vero reiciendis fugit dolorem eos? Non dignissimos architecto ipsum?
+    </p>
+  </article>;
 }
